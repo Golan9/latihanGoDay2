c413bd791c38a5927f9693203bb0d6dd49bc6a53
Damia <damiaralitsa@gmail.com>
2023-02-07 18:09:00 +0700

    implement functions and call on main

--- domain/function.go
+++ domain/function.go
@@ -2,7 +2,63 @@ package domain
 
 import (
 	"fmt"
+	"math"
 )
+
+func (l Lingkaran) Luas() float64 {
+	jariJari := l.Diameter / 2
+	return math.Pi * math.Pow(jariJari, 2)
+}
+
+func (l Lingkaran) Keliling() float64 {
+	return math.Pi * l.Diameter
+}
+
+func (p Persegi) Luas() float64 {
+	return p.Sisi * p.Sisi
+}
+
+func (p Persegi) Keliling() float64 {
+	return 4 * p.Sisi
+}
+
+// interface kosong
+func InterfaceKosong() {
+	fmt.Println("-interface kosong-")
+	var k Kosong
+	k = "dam"
+	fmt.Println("interface string:", k)
+	k = 12.4
+	var i float64 = 5.5
+	result := i * k.(float64)
+	fmt.Println("interface int:", k)
+	fmt.Println("interface float:", result)
+	k = []string{"dam", "dim"}
+	fmt.Println("interface slice:", k)
+}
+
+// -function as parameter-
+func GetMoreThanSix(number int) bool {
+	return number > 6
+}
+
+func Filter(numbers []int, callback func(int) bool) (result []int) {
+	for _, value := range numbers {
+		if filtered := callback(value); filtered {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+// function with multiple return
+func SetPersonalData(n string, l string, a int) PersonalData {
+	var p PersonalData
+	p.Name = n
+	p.Location = l
+	p.Age = a
+	return p
+}
 
 func Test() {
 	fmt.Println("hello world")
--- main/main.go
+++ main/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"latihanGoDay2/domain"
 )
 
 func main() {
+	fmt.Println("-function with multiple return struct-")
+	fmt.Println(domain.SetPersonalData("dam", "jakarta", 27))
+	fmt.Println("")
+	fmt.Println("-interface and function receiver implementation-")
+	var h domain.Hitung
+	h = domain.Lingkaran{10}
+	fmt.Println("luas lingkaran:", h.Luas(), "keliling lingkaran:", h.Keliling())
+	h = domain.Persegi{5}
+	fmt.Println("luas persegi:", h.Luas(), "keliling lingkaran:", h.Keliling())
+	fmt.Println("")
+	domain.InterfaceKosong()
+	fmt.Println("")
+	fmt.Println("-function as parameter-")
+	var numbers = []int{0, 1, 2, 6, 3, 7, 4, 8, 16, 22}
+	var result = domain.Filter(numbers, domain.GetMoreThanSix)
+	fmt.Println("angka lebih besar dari 6 :", result)
+	fmt.Println("")
 	domain.Test()
 }

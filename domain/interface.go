3d1e322c7d44426ed4979687bfe7d32a509e9ba2
Damia <damiaralitsa@gmail.com>
2023-02-07 17:54:57 +0700

    set struct, interface, and function on module domain

---
+++ domain/interface.go
@@ -0,0 +1,10 @@
+package domain
+
+// -interface-
+type Hitung interface {
+	Luas() float64
+	Keliling() float64
+}
+
+//interface kosong
+type Kosong interface{}
---
+++ domain/struct.go
@@ -0,0 +1,15 @@
+package domain
+
+type PersonalData struct {
+	Name     string
+	Location string
+	Age      int
+}
+
+type Lingkaran struct {
+	Diameter float64
+}
+
+type Persegi struct {
+	Sisi float64
+}
